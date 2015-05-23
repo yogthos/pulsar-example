16947dd45e0e5b97e6524ad405a55bfb72ee4255
Yogthos <dmitri.sotnikov@gmail.com>
2015-05-23 00:03:44 -0400

    updated build settings

--- project.clj
+++ project.clj
@@ -4,16 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [co.paralleluniverse/pulsar "0.6.2"]]
+                 [co.paralleluniverse/pulsar "0.6.2"]
+                 [co.paralleluniverse/quasar-core "0.6.2"]]
 
-    :manifest {"Premain-Class" "co.paralleluniverse.fibers.instrument.JavaAgent"
-               "Agent-Class" "co.paralleluniverse.fibers.instrument.JavaAgent"
-               "Can-Retransform-Classes" "true"
-               "Can-Redefine-Classes" "true"}
+  :manifest {"Premain-Class" "co.paralleluniverse.fibers.instrument.JavaAgent"
+             "Agent-Class" "co.paralleluniverse.fibers.instrument.JavaAgent"
+             "Can-Retransform-Classes" "true"
+             "Can-Redefine-Classes" "true"}
   
   :java-agents [[co.paralleluniverse/quasar-core "0.6.2"]]
 
- :main pulsar-example.core
-
- :aot [pulsar-example.core])
+  :main pulsar-example.core
+  :uberjar-name "pulsar-example.jar" 
+  :aot [pulsar-example.core])
 
