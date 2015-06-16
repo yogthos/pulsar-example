87c6f7d34359f128dab2f6312f98c8edc894eb66
Yogthos <dmitri.sotnikov@uhn.on.ca>
2015-06-16 16:58:49 -0400

    updated Clojure version, added description and url

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject pulsar-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "a sample project using Pulsar"
+  :url "https://github.com/yogthos/pulsar-example"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [co.paralleluniverse/pulsar "0.7.0"]
                  [co.paralleluniverse/quasar-core "0.7.0"]]
 
